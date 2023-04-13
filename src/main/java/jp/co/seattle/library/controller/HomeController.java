415b5bc3c8edfb00355c86c25799412450345be8
saito <saito@localhost>
2023-04-12 19:58:37 -0700

    task3まーちゃん

--- src/main/java/jp/co/seattle/library/controller/HomeController.java
+++ src/main/java/jp/co/seattle/library/controller/HomeController.java
@@ -1,5 +1,7 @@
 package jp.co.seattle.library.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.seattle.library.dto.BookInfo;
 import jp.co.seattle.library.service.BooksService;
 
 /**
@@ -29,7 +32,13 @@ public class HomeController {
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String transitionHome(Model model) {
 		//書籍の一覧情報を取得（タスク３）
-
+		//booksテーブルから書籍情報を取得する
+		//書籍の一覧を表示する
+		//一覧画面に遷移後、書籍の一覧を書籍名の昇順で表示させる
+		List<BookInfo> book = booksService.getBookList();
+		//情報を取得しました。
+		model.addAttribute("bookList", book);
+		//bookListにbookの値を飛ばす。bookListはjspに書いたやつ
 		return "home";
 	}
 
