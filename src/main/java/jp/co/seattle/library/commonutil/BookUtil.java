d46d6dbe2874fa6cf43f5095a50f942534da5182
saito <saito@localhost>
2023-04-14 16:00:17 -0700

    task4_1次コミット

--- src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
+++ src/main/java/jp/co/seattle/library/commonutil/BookUtil.java
@@ -17,6 +17,7 @@ public class BookUtil {
 	private static final String REQUIRED_ERROR = "未入力の必須項目があります";
 	private static final String ISBN_ERROR = "ISBNの桁数または半角数字が正しくありません";
 	private static final String PUBLISHDATE_ERROR = "出版日は半角数字のYYYYMMDD形式で入力してください";
+	//addの74の情報がここにくる
 
 	/**
 	 * 登録前のバリデーションチェック
@@ -25,17 +26,25 @@ public class BookUtil {
 	 * @return errorList エラーメッセージのリスト
 	 */
 	public List<String> checkBookInfo(BookDetailsInfo bookInfo) {
-		
+
 		//TODO　各チェックNGの場合はエラーメッセージをリストに追加（タスク４）
 		List<String> errorList = new ArrayList<>();
 		// 必須チェック
+		if (isEmptyBookInfo(bookInfo) == false) {
+			//89行メソッドの呼び出し、！で反転
+			errorList.add(REQUIRED_ERROR);
+		}
 
-		
 		// ISBNのバリデーションチェック
+		//他のとこで似た書き方を見つける
+		if (isValidIsbn(bookInfo.getIsbn()) == false) {
+			errorList.add(ISBN_ERROR);
+		}
 
-
-		// 出版日の形式チェック
-
+		// 出版日の形式チェック  
+		if (checkDate(bookInfo.getPublishDate()) == false) {
+			errorList.add(PUBLISHDATE_ERROR);
+		}
 
 		return errorList;
 	}
@@ -51,7 +60,7 @@ public class BookUtil {
 			DateFormat formatter = new SimpleDateFormat("yyyyMMdd");
 			formatter.setLenient(false); // ←これで厳密にチェックしてくれるようになる
 			//TODO　取得した日付の形式が正しければtrue（タスク４）
-			
+			formatter.parse(publishDate);
 			return true;
 		} catch (Exception p) {
 			p.printStackTrace();
@@ -66,9 +75,15 @@ public class BookUtil {
 	 * @return ISBNが半角数字で10文字か13文字かどうか
 	 */
 	private static boolean isValidIsbn(String isbn) {
-		//TODO　ISBNが半角数字で10文字か13文字であればtrue（タスク４）
-		
+		//isValidIsbnがメソッド名、34行で呼び出される
+		if (isbn.length() > 0) {
+			if (isbn.matches("^[0-9]{10}||{13}+$")) {
+				return true;
+			}
+			return false;
+		}
 		return true;
+		//0-9が半角数字、かつ、10桁か13桁
 	}
 
 	/**
@@ -79,7 +94,11 @@ public class BookUtil {
 	 */
 	private static boolean isEmptyBookInfo(BookDetailsInfo bookInfo) {
 		//TODO　タイトル、著者、出版社、出版日のどれか一つでもなかったらtrue（タスク４）
-		
-		return true;
+		//if((!bookInfo.getTitle().isEmpty())){メソッドを入れなきゃいけないから違う方法模索
+		if (bookInfo.getTitle().length() != 0 && bookInfo.getAuthor().length() != 0
+				&& bookInfo.getPublisher().length() != 0 && bookInfo.getPublishDate().length() != 0) {
+			return true;
+		}
+		return false;
 	}
 }
